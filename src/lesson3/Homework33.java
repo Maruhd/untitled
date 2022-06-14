8c06a2a2d54ab974dcfe92d9d80f3065af885e18
Пашв <pavlik.kozlov.1993@gmail.com>
2022-06-14 21:54:28 +0300

    HomeWorkNumber3

---
+++ src/lesson3/Homework33.java
@@ -0,0 +1,29 @@
+package lesson3;
+
+import java.util.Scanner;
+
+public class Homework33 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Entem numbers color");
+        int n = scan.nextInt();
+        if (n == 1) {
+            System.out.println("Red");
+        } else if (n == 2) {
+            System.out.println("Orange");
+        } else if (n == 3) {
+            System.out.println("Yellow");
+        } else if (n == 4) {
+            System.out.println("Green");
+        } else if (n == 5) {
+            System.out.println("blue");
+        } else if (n == 6) {
+            System.out.println("dark blue");
+        } else if (n == 7) {
+            System.out.println("Purple");
+        } else {
+            System.out.println("color number does not exist");
+        }
+    }
+
+}
---
+++ src/lesson3/SqrtNubers.java
@@ -0,0 +1,12 @@
+package lesson3;
+
+public class SqrtNubers {
+    public static void main(String[] args) {
+        for (int i = 10; i <= 20; i++) {
+            double b = Math.pow(i, 2);
+            System.out.println(Math.round(b));
+        }
+    }
+}
+
+
